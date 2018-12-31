ffcc5ab8c5d83bbedcd0fa5448cfbd73ae990d43
George Spyrou <gspyrou@umich.edu>
2018-12-31 10:20:50 +0000

    Update ann_.py

--- ann_.py
+++ ann_.py
@@ -42,3 +42,10 @@ X = X[:,1:]
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Each important to perform Feature scaling before we fit a NN
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
