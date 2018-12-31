d2ba3a5a6db510f0cbf2b767614b6acec5870195
George Spyrou <gspyrou@umich.edu>
2018-12-31 10:17:50 +0000

    Update ann_.py

--- ann_.py
+++ ann_.py
@@ -4,9 +4,15 @@
 # Part 1 - Data Preprocessing
 
 # Importing the libraries
+
+# Data processing
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+
+# Visualization
+import matplotlib.pyplot as plt
+
+
 
 # Goal: Predict which costumers are going to leave the bank (binary classification)
 
@@ -17,3 +23,19 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 
 X = dataset.iloc[:, 3:13].values # contains thwe useful features
 y = dataset.iloc[:, 13].values # contains the binary outcomes
+
+# We have some categorical variables, so at first point we need to 
+# encode the categorical data
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+
+label_encoder_1 = LabelEncoder()
+X[:,1] = label_encoder_1.fit_transform(X[:,1]) # Encoding Countries
+
+label_encoder_2 = LabelEncoder()
+X[:,2] = label_encoder_2.fit_transform(X[:,2]) # Encoding Sex
+
+# Create dummy variables for Countries
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:,1:]
