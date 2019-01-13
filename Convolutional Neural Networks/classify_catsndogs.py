594f70fc770863fc106f52cc004fd30157feb613
George Spyrou <gspyrou@umich.edu>
2019-01-13 13:05:31 +0000

    added the Data Augmentation step

--- Convolutional Neural Networks/classify_catsndogs.py
+++ Convolutional Neural Networks/classify_catsndogs.py
@@ -45,5 +45,38 @@ classifier.add(Dense(output_dim = 1 , activation = 'sigmoid' ))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 
+# Preprocessing
 
+# Image Augmentation: Create new images as our training set is pretty low.
+# It allows us to enrich our dataset, and prevent overfitting.
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_path = 'C:\\Users\\hz336yw\\Desktop\\Personal\\Udemy\\Deep_Learning_A_Z\\Deep_Learning_A_Z\\Convolutional_Neural_Networks\\Convolutional_Neural_Networks\\dataset\\training_set'
+test_path = 'C:\\Users\\hz336yw\\Desktop\\Personal\\Udemy\\Deep_Learning_A_Z\\Deep_Learning_A_Z\\Convolutional_Neural_Networks\\Convolutional_Neural_Networks\\dataset\\test_set'
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(train_path,
+                                                target_size = (64, 64),
+                                                batch_size = 32,
+                                                class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory(test_path,
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch = 8000, # number of instances in training set
+        epochs=25,
+        validation_data = test_set,
+        validation_steps = 2000) # number of instances in test set
 
