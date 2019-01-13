e89b15051a775f346927eb3603b1e38f72702147
George Spyrou <gspyrou@umich.edu>
2019-01-13 12:19:17 +0000

    added the fully connected layer

--- Convolutional Neural Networks/classify_catsndogs.py
+++ Convolutional Neural Networks/classify_catsndogs.py
@@ -36,6 +36,10 @@ classifier.add(MaxPool2D(pool_size = (2,2)))
 # We are flatteting all the Pooling Layers into one huge vector that will be the input to the ANN
 classifier.add(Flatten())
 
+# Step 4 - Full Connection
+classifier.add(Dense(output_dim = 128 , activation = 'relu' ))
+# sigmoid as we have binary outcome/ if we had more than 2 then we use softmax activation function
+classifier.add(Dense(output_dim = 1 , activation = 'sigmoid' )) 
 
 
 
