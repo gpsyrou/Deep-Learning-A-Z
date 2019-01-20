42bd72de454544c2d1eb550a71d36242358a4792
George Spyrou <gspyrou@umich.edu>
2019-01-20 12:49:35 +0000

    Update recurrent_nn.py

--- Recurrent Neural Networks/recurrent_nn.py
+++ Recurrent Neural Networks/recurrent_nn.py
@@ -8,8 +8,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 os.chdir("C:\\Users\\hz336yw\\Desktop\\Personal\\Udemy\\Deep_Learning_A_Z\\Deep_Learning_A_Z\\Recurrent_Neural_Networks-1\\Recurrent_Neural_Networks")
-
-
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:,1:2].values # Picking only the opening price as a numpy array
 
