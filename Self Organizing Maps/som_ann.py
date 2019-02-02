81d5964521f67f97c205e348663bdbe0f5339dfa
George Spyrou <gspyrou@umich.edu>
2019-02-02 16:32:00 +0000

    Update som_ann.py

--- Self Organizing Maps/som_ann.py
+++ Self Organizing Maps/som_ann.py
@@ -88,3 +88,4 @@ classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
 y_pred = classifier.predict(customers)
 y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
 y_pred = y_pred[y_pred[:, 1].argsort()]
+y_pred
