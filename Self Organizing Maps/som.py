3f5ca8e20c43d06fd0f3c013e42e893900980fc2
George Spyrou <gspyrou@umich.edu>
2019-02-02 15:42:59 +0000

    Update som.py

--- Self Organizing Maps/som.py
+++ Self Organizing Maps/som.py
@@ -11,3 +11,15 @@ import pandas as pd
 # Each line of the dataset corresponds to a customer. Thus we will try to find  segments of customers
 dataset = pd.read_csv(r'C:\Users\george\Desktop\Online Courses\Udemy\Self_Organizing_Maps\Credit_Card_Applications.csv')
 dataset.head()
+
+# For each customer the winning node is the node which is most similar with the customer
+
+# We will split the dataset into two subsets: one containing customers whose application got approved, and another one with customers whose application got declined
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+# Feature scalling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1)) # scalling so that the values all fall between 0 and 1
+
+X = sc.fit_transform(X)
